3175013e45c00da22c75e01ea1e4e1019b8d833b
Elías García Mariño <elias.garcia@netexlearning.com>
2019-03-18 17:12:24 +0100

    gulpfile test

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,20 @@
-function defaultTask(cb) {
-  // place code for your default task here
-  cb();
-}
+const gulp = require('gulp');
+const del = require('del');
+const { exec } = require('child_process');
 
-exports.default = defaultTask;
+gulp.task('build', function() {
+  const buildProcess = exec('"node" ./node_modules/typescript/bin/tsc -b tsproject.json');
+
+  buildProcess.stdout.pipe(process.stdout);
+
+  return buildProcess;
+});
+
+gulp.task('watch', gulp.series('build'), function() {
+  gulp.watch('./*.ts', ['build']);
+});
+
+gulp.task('clean', () => {
+  return del('dist');
+});
+

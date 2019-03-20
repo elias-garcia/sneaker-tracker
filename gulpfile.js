291a0dcb8082775512853e2f6e718356aaea5b6f
elias-garcia <eliasgarciadev@gmail.com>
2019-03-20 21:19:49 +0100

    gulp refactor

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,47 @@
-const gulp = require('gulp');
-const del = require('del');
+const { watch, parallel } = require('gulp');
+const { join } = require('path');
 const { exec } = require('child_process');
+const del = require('del');
 
-gulp.task('build', function() {
-  const buildProcess = exec('"node" ./node_modules/typescript/bin/tsc -b tsproject.json');
+const API_PATH = join(__dirname, './api');
+const SCRAPER_PATH = join(__dirname, './scraper');
 
-  buildProcess.stdout.pipe(process.stdout);
+// HELPER FUNCTIONS
+const execCmd = (cmd) => {
+  const execProcess = exec(cmd);
 
-  return buildProcess;
-});
+  execProcess.stdout.pipe(process.stdout);
 
-gulp.task('watch', gulp.series('build'), function() {
-  gulp.watch('./*.ts', ['build']);
-});
+  return execProcess;
+}
 
-gulp.task('clean', () => {
-  return del('dist');
-});
+const installDependencies = (path) => {
+  return execCmd(`"npm" install --prefix ${path}`);
+}
 
+// gulp.task('watch', gulp.series('build'), function() {
+//   gulp.watch('./*.ts', ['build']);
+// });
+
+// DEPENDENCIES TASKS
+const installApiDeps = () => installDependencies(API_PATH);
+const installScraperDeps = () => installDependencies(SCRAPER_PATH);
+const installProjectDeps = (done) => {
+  return parallel(installApiDeps, installScraperDeps)(done);
+};
+
+// CLEAN TASKS
+const clean = () => del('dist');
+
+// BUILD TASKS
+const buildProject = () => execCmd('"node" ./node_modules/typescript/bin/tsc -b tsproject.json');
+const watchProject = () => watch(['./**/*.ts', '!./**/node_modules/**'], buildProject);
+
+module.exports = {
+  buildProject,
+  clean,
+  installApiDeps,
+  installScraperDeps,
+  installProjectDeps,
+  watchProject
+}

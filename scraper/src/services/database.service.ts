c646ddc8b1b26d3507c131bc44b89327e632c3ae
elias-garcia <eliasgarciadev@gmail.com>
2019-04-11 16:38:25 +0200

    database service for sneaker price bulk

--- scraper/src/services/database.service.ts
+++ scraper/src/services/database.service.ts
@@ -1,26 +1,54 @@
 import { BulkWriteOpResultObject } from "mongodb";
-import { Sneaker } from "shared/models";
+import { Sneaker, SneakerPrice } from "shared/models";
 import { Sneaker as ISneaker } from "../interfaces/sneaker.interface";
+
+function createSneakerBulkOp(
+  sneaker: ISneaker,
+  shopId: string,
+): any {
+  return {
+    updateOne: {
+      filter: { "ref": sneaker.ref, "sizesData.shop": shopId },
+      update: {
+        $set: {
+          "sizesData.$": { $max: { mostRecentPrice: sneaker.price } },
+        },
+      },
+      upsert: true,
+    },
+  };
+}
+
+function createSneakerPriceBulkOp(
+  sneaker: ISneaker,
+  shopId: string,
+): any {
+  return {
+    insertOne: {
+      document: {
+        sneaker: sneaker.ref,
+        shop: shopId,
+        price: sneaker.price,
+        currency: sneaker.currency,
+      },
+    },
+  };
+}
 
 export function saveSneakers(
   sneakers: ISneaker[],
   shopId: string,
-): Promise<BulkWriteOpResultObject> {
-  const bulkOps = [];
+): Array<Promise<BulkWriteOpResultObject>> {
+  const sneakerBulkOps = [];
+  const sneakerPriceBulkOps = [];
 
   for (const sneaker of sneakers) {
-    bulkOps.push({
-      updateOne: {
-        filter: { "ref": sneaker.ref, "sizesData.shop": shopId },
-        update: {
-          $set: {
-            "sizesData.$": { $max: { mostRecentPrice: sneaker.price } },
-          },
-        },
-        upsert: true,
-      },
-    });
+    sneakerBulkOps.push(createSneakerBulkOp(sneaker, shopId));
+    sneakerPriceBulkOps.push(createSneakerPriceBulkOp(sneaker, shopId));
   }
 
-  return Sneaker.bulkWrite(bulkOps);
+  return [
+    Sneaker.bulkWrite(sneakerBulkOps),
+    SneakerPrice.bulkWrite(sneakerPriceBulkOps),
+  ];
 }
